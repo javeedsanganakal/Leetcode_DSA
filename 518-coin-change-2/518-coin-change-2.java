1add5ff6fb9514e628838a544d2e90a902a6a355
Javeed Sanganakal <21953322+javeedsanganakal@users.noreply.github.com>
2022-07-30 13:51:51 -0700

    Time: 11 ms (54.03%), Space: 41.3 MB (86.89%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.java
+++ 518-coin-change-2/518-coin-change-2.java
@@ -4,28 +4,23 @@ class Solution {
         //null
         if(coins ==  null || coins.length == 0) return 0;
         int m = coins.length; int n = amount;
-        int [][] dp = new int[m+1][n+1];
-        dp[0][0] = 1;
+        int [] dp = new int[n+1];
+        dp[0] = 1;
         
         for(int i=1; i<= m;  i++){
             for(int j =0; j<=n; j++){
                 if(j<coins[i-1]){
                     
-                    dp[i][j] = dp[i-1][j];
+                    dp[j] = dp[j];
                 } 
                 else{
-                    dp[i][j] = dp[i-1][j] + dp[i][j-coins[i-1]];
+                    dp[j] = dp[j] + dp[j-coins[i-1]];
                 }
             }
         }
+
+        return dp[n];
         
-        
-        return dp[m][n];
-        
-              
     }
-    
-   
-    
-    
+       
 }
