8dccc9ca54e32b7b11eb22902733e7c4677ec730
Javeed Sanganakal <21953322+javeedsanganakal@users.noreply.github.com>
2022-08-03 07:29:43 -0700

    Time: 0 ms (100.00%), Space: 42.4 MB (50.70%) - LeetHub

--- 289-game-of-life/289-game-of-life.java
+++ 289-game-of-life/289-game-of-life.java
@@ -14,24 +14,28 @@ class Solution {
         //                   top  bottom    left    right   topLeft   topRight  bottomLeft  bottomRight
         dirs = new int[][]{{-1,0}, {1,0},  {0,-1},  {0,1},  {-1,-1},   {-1,1},    {1,-1},      {1,1} };
         
-        for(int i=0; i<m; i++){
-            for(int j=0; j<n; j++){
-                int countlives = countAlive(board, i, j);
+        for(int i=0; i<m; i++){ //no of rows
+            for(int j=0; j<n; j++){ // no of cols
+                
+                int countlives = countAlive(board, i, j); // get count of live cells from 8 all directions arround that particular cell
 
-                //2 No need implement
+                //2nd Rule, No need implement
                 //1th and 3rd Rule
                 if(board[i][j] == 1 && (countlives < 2 ||  countlives > 3)){
+                    //Changing the state of cell temporarily
                     board[i][j] = 2;
                 }
                 
                 //4th Rule
                 if(board[i][j] == 0 && countlives == 3){
+                    //Changing the state of cell temporarily
                     board[i][j] = 3;
                 }
             }
         }
         
-        for(int i=0; i<m; i++){
+        //Changing the state of cells temporarily to atcual changed values
+        for(int i=0; i<m; i++){ 
             for(int j=0; j<n; j++){
                 if(board[i][j] == 2) board[i][j] = 0;
                 if(board[i][j] == 3) board[i][j] = 1;
@@ -41,12 +45,16 @@ class Solution {
     
     private int countAlive(int [][] board, int r, int c){
         int count = 0;
+        //Get all directions and iterate over them
         for(int []dir : dirs){
+            //Adding current row and direction row
             int nr = r + dir[0];  //{-1,0} ==> -1
+             //Adding current col and direction col
             int nc = c + dir[1];  //{-1,0} ==>  0
             
             //borders of columns and rows. condition without crossing
             // nr ==> 0 to m   and  cr ==> 0 to n
+            
             if(nr >= 0 && nr < m && nc >= 0 && nc < n
               && (board[nr][nc]==1 || board[nr][nc] == 2)){
                 count ++;
