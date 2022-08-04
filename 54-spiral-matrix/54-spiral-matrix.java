e41dcefcd5394468339fa03c22afed80d0415b77
Javeed Sanganakal <21953322+javeedsanganakal@users.noreply.github.com>
2022-08-04 07:56:07 -0700

    Time: 0 ms (100.00%), Space: 42 MB (56.63%) - LeetHub

--- 54-spiral-matrix/54-spiral-matrix.java
+++ 54-spiral-matrix/54-spiral-matrix.java
@@ -1,49 +1,44 @@
 class Solution {
+    
+    //Recursive
+    List<Integer> result=new ArrayList<>();
     public List<Integer> spiralOrder(int[][] matrix) {
-        int m = matrix.length; 
-        int n = matrix[0].length;
-        int left = 0; int right = n-1;
-        int top = 0; int bottom = m-1;
-        
-        List<Integer> result = new ArrayList<>();
-        while(top <= bottom && left <= right){
-            
-            
-            
-            
-            //top row
-
-                for(int j=left; j<= right ; j++){
-                result.add(matrix[top][j]);  
-               }
-            top++;
-            
-            //right column
-                 for(int i=top; i<= bottom ; i++){
-                result.add(matrix[i][right]);   
-              }
-            right--;
-            
-            
-            //bottom row
-            if(top <= bottom){
-                  for(int k=right; k >= left ; k--){
-                result.add(matrix[bottom][k]);  
-                  }
-             }
-            bottom--;
-            
-            //left column
-             if(left <= right){
-                 for(int l=bottom; l >= top ; l--){
-                result.add(matrix[l][left]);
-                 }
-             }
-            
-            left++;
-            
-        }
+        if(matrix.length == 0) return result;
+        helper(matrix, 0, matrix[0].length - 1, 0, matrix.length - 1);
         return result;
-        
     }
+
+    public void helper(int[][] matrix, int left, int right, int top, int bottom){
+        // Base case : If pointers cross each other terminate
+        if(left > right || top > bottom) return;
+        // top row
+        for(int i = left; i <= right; i++){
+            result.add(matrix[top][i]);
+        }
+        top++;
+        //right column
+        for(int i = top; i <= bottom; i++){
+            result.add(matrix[i][right]);
+        }
+        right--;
+        // We need to check top <= bottom because original position of top has increased
+        if(top <= bottom){
+              //Bottom row
+            for(int i = right; i >= left; i--){
+                result.add(matrix[bottom][i]);
+            }
+        }
+        bottom--;
+           // We need to check left <= right because original position of left has changed
+        if(left <= right){
+              //left column
+            for(int i = bottom; i >= top; i--){
+                result.add(matrix[i][left]);
+            }
+        }
+        left++;
+        helper(matrix,left, right, top, bottom); 
+
+    }
+
 }
