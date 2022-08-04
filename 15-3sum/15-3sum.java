f80cb1acabfb11c12299966e94567fe4d035fce5
Javeed Sanganakal <21953322+javeedsanganakal@users.noreply.github.com>
2022-08-04 15:21:18 -0700

    Time: 24 ms (94.15%), Space: 46.4 MB (93.96%) - LeetHub

--- 15-3sum/15-3sum.java
+++ 15-3sum/15-3sum.java
@@ -1,18 +1,20 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         
-        
-        
+        //Declare List of Integers List
         List<List<Integer>> result = new ArrayList<>();
+        
         //return the empty ArrayList
         if(nums == null || nums.length == 0) return new ArrayList<>();
         int n = nums.length;
         
-        
+        //Sort the unsorted Array O(nlogn)
         Arrays.sort(nums);
         
+        //loop till 3rd num from last
         for(int i=0; i < n-2; i++){
             
+            // if
             if(i!=0 && nums[i] == nums[i-1]) continue;
             
             int low = i+1;
@@ -22,26 +24,32 @@ class Solution {
                 int sum = nums[i] + nums[low] + nums[high];
                 
                 if(sum == 0) {
+                    //List of Integers
                     List<Integer> li = Arrays.asList(nums[i], nums[low], nums[high]);
+                    
+                    // Sorted array no need to sort and add to list
                     result.add(li);  
+                    
+                    //Inc low pointer
                     low++; 
+                    //Dec high pointer
                     high--;
+                    
+                    //Duplicates from left side, increase the low pointer
                     while(low<high && nums[low] == nums[low-1]) low ++;
+                     //Duplicates from right side, decrease the low pointer
                     while(low<high && nums[high] == nums[high+1]) high--;
                 }
+                //if sum is greater thaan target, decr the high to get the target
                 else if(sum > 0){
                     high--;
                 }
+                 //if sum is smaller thaan target, incr the low to get the target
                 else{
                     low++;
                 }
             }
         }
-        
-        
-     
-       
-        
         
         return result; 
     }
