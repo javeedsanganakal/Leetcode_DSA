5fc51288dcedaa903c71f54691e37853f25f28f0
Javeed Sanganakal <21953322+javeedsanganakal@users.noreply.github.com>
2022-08-20 23:08:52 -0700

    Time: 2 ms (87.25%), Space: 42.3 MB (96.98%) - LeetHub

--- 515-find-largest-value-in-each-tree-row/515-find-largest-value-in-each-tree-row.java
+++ 515-find-largest-value-in-each-tree-row/515-find-largest-value-in-each-tree-row.java
@@ -13,46 +13,21 @@
  *     }
  * }
  */
-class Solution {
-	public List<Integer> largestValues(TreeNode root) {
-		List<Integer> ans = new ArrayList<>();
-
-		if(root == null) return ans;
-
-		//declare queue for level order traversal
-		Queue<TreeNode> q = new LinkedList<>();
-		q.offer(root);
-
-		// loop till queue is not empty
-		while(!q.isEmpty()) {
-			int size = q.size();
-
-			// store max element for each tree row
-			int max = Integer.MIN_VALUE;
-
-
-			for(int i = 0;i<size;i++) {
-				TreeNode node = q.peek();
-
-				// if there is left child, add it to the queue
-				if(node.left != null) {
-					q.add(node.left);
-				}
-				
-				// if there is right child, add it to the queue
-				if(node.right != null) {
-					q.add(node.right);
-				}
-				
-				// check if the curr node value is greater than max, if so update max, keep doing this for all elements
-				// in the row
-				max = Math.max(max, node.val);
-				q.poll();
-			}
-			// lastly add that max value in the ans list
-			ans.add(max);
-		}
-
-		return ans;
-	}
+public class Solution {
+    public List<Integer> largestValues(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        Queue<TreeNode> q = new LinkedList<>();
+        if (root != null) q.offer(root);
+        while (!q.isEmpty()) {
+            int size = q.size(), max = Integer.MIN_VALUE;
+            for (int i = 0; i < size; i++) {
+                TreeNode node = q.poll();
+                max = Math.max(max, node.val);
+                if (node.left != null) q.offer(node.left);
+                if (node.right != null) q.offer(node.right);
+            }
+            res.add(max);
+        }
+        return res;
+    }
 }
