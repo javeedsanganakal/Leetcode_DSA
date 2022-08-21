36f65b61346ec59061b021266ecc08a959d2e01b
Javeed Sanganakal <21953322+javeedsanganakal@users.noreply.github.com>
2022-08-20 19:15:04 -0700

    Time: 1 ms (64.31%), Space: 42.6 MB (32.89%) - LeetHub

--- 101-symmetric-tree/101-symmetric-tree.java
+++ 101-symmetric-tree/101-symmetric-tree.java
@@ -14,32 +14,30 @@
  * }
  */
 
-//DFS 
+//BFS using Queue
 
+//TC = O(n)
+//SC = O(n)
 class Solution {
-    boolean flag;
     public boolean isSymmetric(TreeNode root) {
-        if(root == null) return true;
-        flag = true;
-        dfs(root.left, root.right);
-        return flag;
-    }
-    
-    private void dfs(TreeNode left, TreeNode right){
-        //base
-        if(left == null && right == null) return;
+       if(root == null) return true;
+       Queue<TreeNode> q = new LinkedList<>();
+       q.add(root.left);
+       q.add(root.right);
         
-        if(left == null || right == null || left.val != right.val){
-            flag = false;
-            return;
+        while(!q.isEmpty()){
+            TreeNode left = q.poll();
+            TreeNode right = q.poll();
+            
+            if(left == null && right == null) continue;
+            if(left == null || right == null || left.val != right.val) return false;
+            
+            q.add(left.left); q.add(right.right);
+            q.add(left.right); q.add(right.left);
+            
+            
         }
         
-        
-        //logic
-        dfs(left.left, right.right);
-        //st.pop
-        dfs(left.right, right.left);
-        //st.pop
-   
+        return true;
     }
 }
