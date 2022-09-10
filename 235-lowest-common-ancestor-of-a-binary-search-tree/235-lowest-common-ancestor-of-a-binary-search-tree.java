d85ea1d11a755e67aca103dc58fff4aef1398a0f
Javeed Sanganakal <21953322+javeedsanganakal@users.noreply.github.com>
2022-09-10 15:56:38 -0700

    Time: 4 ms (100.00%), Space: 43 MB (96.92%) - LeetHub

--- 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.java
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.java
@@ -8,14 +8,22 @@
  * }
  */
 
-//Approach - 1 : Recursive 
+//Approach - 2 : Iterative by using while loop, no extra space bcz we are not using recursion
 //Time Complexity : O(h) // logarithemic
-//Space Complexity : O(h) // Stack calls
+//Space Complexity : 
 
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if(root.val < p.val && root.val < q.val) return lowestCommonAncestor(root.right, p, q);
-        else if(root.val > p.val && root.val > q.val) return lowestCommonAncestor(root.left, p, q);
+        
+        
+        while(true){
+        //BST all nodes are left is less than root
+        if(root.val < p.val && root.val < q.val) root =  root.right;
+        //BST all nodes are left is greater than root
+        else if(root.val > p.val && root.val > q.val) root = root.left;
+        //if one node is on left and other on right, then LCA is root itself
         else return root;
-    }
+        }
+   }
+
 }
