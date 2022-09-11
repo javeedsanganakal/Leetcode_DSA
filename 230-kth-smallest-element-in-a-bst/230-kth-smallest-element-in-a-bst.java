f85d57e7ea7d8133d5a203e607b88ef08afc4f50
Javeed Sanganakal <21953322+javeedsanganakal@users.noreply.github.com>
2022-09-10 19:39:51 -0700

    Time: 12 ms (5.17%), Space: 45.7 MB (33.16%) - LeetHub

--- 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.java
+++ 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.java
@@ -14,12 +14,13 @@
  * }
  */
 
-//Approach - 1:  Recursion, Inorder and decrement count=k 
+//Approach - 1:  Conditinal Recursive calls, Inorder and decrement count=k 
 //Time Complexity : o(n)
 //Space Complexity : O(h)
 class Solution {
     int count;
     int result;
+    boolean flag;
     public int kthSmallest(TreeNode root, int k) {
         if(root == null) return -1;
         count = k;
@@ -32,14 +33,17 @@ class Solution {
     if(root == null) return;
     
     //logic
-    inorder(root.left);
+    if(!flag)
+     inorder(root.left);
     System.out.println(root.val);
     count --;
     if(count == 0){
+        flag = true;
         result = root.val;
         return;
-    }     
-    inorder(root.right);
+    } 
+    if(!flag)
+     inorder(root.right);
     }
  
 }
