a0af6cc8d8db25d8ff12c5e05922259393975048
Javeed Sanganakal <21953322+javeedsanganakal@users.noreply.github.com>
2022-09-16 15:22:30 -0700

    Time: 12 ms (19.03%), Space: 48.5 MB (53.69%) - LeetHub

--- 155-min-stack/155-min-stack.java
+++ 155-min-stack/155-min-stack.java
@@ -1,30 +1,28 @@
-//Approach - 1: 2 stacks-> st and minStack, minValue = Infinity
+//Approach - 2: 1 stack-> st, minValue = Infinity
 //Time Complexity : O(1) 
 //Space Complexity : 
 class MinStack {
     
      Stack<Integer> st;
-     Stack<Integer> minStack;
      int min;
     
     public MinStack() {
         this.min = Integer.MAX_VALUE;
         this.st = new Stack<>();
-        this.minStack= new Stack<>();
-        minStack.push(min);
     }
     
     public void push(int val) { //O(1)
-        min = Math.min(min, val);
-        st.push(val);
-        minStack.push(min);
-        
+        if(min >= val){
+             st.push(min);
+             min = val;
+        }
+        st.push(val);      
     }
     
     public void pop() {
-        st.pop();
-        minStack.pop();
-        min = minStack.peek();
+        if(min == st.pop()){
+            min = st.pop(); // pop 2 times if min is pop
+        }
     }
     
     public int top() {
