66bbb782ebdc4589ad9191d1f79bdf78552507a2
Javeed Sanganakal <21953322+javeedsanganakal@users.noreply.github.com>
2022-09-18 09:12:27 -0700

    Time: 4 ms (80.34%), Space: 46.2 MB (15.67%) - LeetHub

--- 1-two-sum/1-two-sum.java
+++ 1-two-sum/1-two-sum.java
@@ -1,42 +1,20 @@
-//Approach 2 : diff= target-nums[i], check diff key in map n get value as idx, pair it or else add to HashMap
+//Technique:2 HashMap 2 Sum
 //Time Complexity =  O(n)
 //Space Complexity = O(n)
 
 class Solution {
-
     public int[] twoSum(int[] nums, int target) {
-
-        Map<Integer,Integer> map= new HashMap<>();
-
-        for(int i=0;i<nums.length;i++)
-
-        {
-
-            map.put(nums[i],i);
-
-        }
-
-        for(int  i=0;i<nums.length;i++)
-
-        {
-
-            int complement=target-nums[i];
-
-            
-            if( map.containsKey(complement) && map.get(complement)!= i)
-
-            {
-
-                return new int [] { i, map.get(complement)};
-
-            }
-
-        }
-
-        return new int [] {-1,-1};
-
+     if(nums == null || nums.length == 0 ) return new int[] {};
+     HashMap<Integer, Integer> map = new HashMap<>(); //O(n)
         
-
-    }
-
+     for(int i=0; i< nums.length ; i++){ //O(n)
+           int diff = target - nums[i];
+         
+           if(map.containsKey(diff)){
+                   return new int[] {map.get(diff), i};          
+           }
+           map.put(nums[i], i);
+     } 
+     return new int[] {-1,-1};
+   }
 }
