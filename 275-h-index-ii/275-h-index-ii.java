8210bf26731fed213517e7141d121dedb50a3ef3
Javeed Sanganakal <21953322+javeedsanganakal@users.noreply.github.com>
2022-09-20 10:36:00 -0700

    Time: 2 ms (16.38%), Space: 51.8 MB (80.37%) - LeetHub

---
+++ 275-h-index-ii/275-h-index-ii.java
@@ -0,0 +1,21 @@
+//Approach - 1: Linear and Binary Search 
+//Time Complexity : O(nlogn)
+//Space Complexity : O(1)
+
+
+//Notes
+
+class Solution {
+    public int hIndex(int[] citations) {
+         if(citations == null || citations.length == 0) return 0;
+        int n = citations.length;
+        for(int i=0; i<n; i++){
+            int diff = n-i;
+            if(diff<=citations[i]){
+                return diff;
+            }
+        }
+        //[0,0,0,0] == [4,3,2,1] no diff<=citations[i] will match
+        return 0;
+    }
+}
