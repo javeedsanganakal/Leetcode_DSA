da3742a34d0efb9e666f6a1dab72f3d49cbf6003
Javeed Sanganakal <21953322+javeedsanganakal@users.noreply.github.com>
2022-09-21 16:46:29 -0700

    Time: 0 ms (100.00%), Space: 52.2 MB (54.62%) - LeetHub

--- 275-h-index-ii/275-h-index-ii.java
+++ 275-h-index-ii/275-h-index-ii.java
@@ -1,5 +1,5 @@
-//Approach - 1: Linear
-//Time Complexity : O(n)
+//Approach - 2: Binary Search
+//Time Complexity : O(logn)
 //Space Complexity : O(1)
 
 
@@ -9,13 +9,23 @@ class Solution {
     public int hIndex(int[] citations) {
          if(citations == null || citations.length == 0) return 0;
         int n = citations.length;
-        for(int i=0; i<n; i++){
-            int diff = n-i;
-            if(diff<=citations[i]){
+        int low = 0;
+        int high = n-1;
+        while(low <= high){
+            int mid = low + (high-low)/2;
+            int diff = n - mid;
+            
+            if(citations[mid] == diff){
                 return diff;
+            }
+            else if(citations[mid] < diff){
+                low = mid+1;
+            }
+            else{
+                high = mid-1;
             }
         }
         //[0,0,0,0] == [4,3,2,1] no diff<=citations[i] will match
-        return 0;
+        return n-low;
     }
 }
