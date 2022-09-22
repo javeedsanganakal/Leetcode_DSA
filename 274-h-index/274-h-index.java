195c6c6f74a7d01fc58f23f10f875038536360f4
Javeed Sanganakal <21953322+javeedsanganakal@users.noreply.github.com>
2022-09-21 18:08:16 -0700

    Time: 1 ms (85.90%), Space: 42.7 MB (8.12%) - LeetHub

--- 274-h-index/274-h-index.java
+++ 274-h-index/274-h-index.java
@@ -1,4 +1,4 @@
-//Approach - 1: Linear and Sort
+//Approach - 2: Linear and Without Sorting by using Bucket Sort concept, Just sort in range
 //Time Complexity : O(nlogn)
 //Space Complexity : O(1)
 
@@ -9,15 +9,28 @@ class Solution {
     public int hIndex(int[] citations) {
         if(citations == null || citations.length == 0) return 0;
         int n = citations.length;
-        //Sort if its not sorted
-        Arrays.sort(citations);
+        //Create an Array of n+1 length, push all elem>=range; 
+        // h index will in array length range, after range number we don't require
+        int[] bucket = new int[n+1];
         for(int i=0; i<n; i++){
-            int diff = n-i;
-            if(diff<=citations[i]){
-                return diff;
+            //if its not in range, just increase the count of last bucket
+            if(citations[i] >= n){
+                bucket[n]++;
+            }
+            //increase particular index and elements count
+            else{
+                bucket[citations[i]]++;
             }
         }
-        //[0,0,0,0] == [4,3,2,1] no diff<=citations[i] will match
+        
+        int sum = 0;
+        for(int i = n; i>=0;i--){
+            //loop from last buckets count 
+            sum = sum+ bucket[i];
+            if(sum >= i){
+                return i;
+            }
+        }
         return 0;
     }
 }
