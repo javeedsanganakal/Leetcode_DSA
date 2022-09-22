02e23b07fd7dced585b1e8dc2560115d7c7020e6
Javeed Sanganakal <21953322+javeedsanganakal@users.noreply.github.com>
2022-09-21 21:38:07 -0700

    Time: 1 ms (92.17%), Space: 43.4 MB (77.82%) - LeetHub

---
+++ 31-next-permutation/31-next-permutation.java
@@ -0,0 +1,44 @@
+//Approach - 1: Breach, find next greater, reverse
+//Time Complexity : O(n)
+//Space Complexity : O(1)
+
+class Solution {
+    public void nextPermutation(int[] nums) {
+        
+        if(nums == null) return;
+        int n = nums.length;
+        
+        //1. find the breach
+        int i = n-2;
+        while(i>=0 && nums[i] >= nums[i+1]){
+            i--;
+        }
+        
+        //2. find the jth elemenet to replace ith element
+        if(i!=-1){
+            int j=n-1;
+            while(nums[i] >= nums[j]){
+                j--;
+            }
+            
+            //swap i and j
+            swap(nums,i,j);
+        }
+        
+        //3. reverse from i+1 to n-1
+        reverse(nums,i+1,n-1);
+    }
+    private void swap(int [] nums, int i, int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+    
+    private void reverse(int [] nums, int left, int right){
+        while(left<right){
+            swap(nums, left, right);
+            left++;
+            right--;
+        }
+    }
+}
