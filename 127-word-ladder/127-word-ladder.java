5e519c977692c4099a6dbc9bb7b13f184dd96b93
Javeed Sanganakal <21953322+javeedsanganakal@users.noreply.github.com>
2022-09-25 18:54:39 -0700

    Time: 171 ms (51.78%), Space: 85.6 MB (47.87%) - LeetHub

---
+++ 127-word-ladder/127-word-ladder.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        HashSet<String> set = new HashSet(wordList);
+        if(!set.contains(endWord)) return 0;
+        Queue<String> queue = new LinkedList<>();
+        queue.offer(beginWord);
+        int level = 1;
+            
+        while(!queue.isEmpty()){
+            int size = queue.size();
+            for(int i = 0 ; i < size ; i++){
+                String currentWord = queue.poll();
+                char currArray []= currentWord.toCharArray();
+                
+                for(int j = 0 ; j < currArray.length ; j++){
+                    char originalChar = currArray[j];
+                    for(char c = 'a' ; c <= 'z' ; c++){
+                        if(currArray[j] == c) continue;
+                        currArray[j] = c;
+                        String newString = String.valueOf(currArray);
+                        //System.out.println(newString);
+                        if(newString.equals(endWord)) return level + 1;
+                        if(set.contains(newString)){
+                            queue.offer(newString);
+                            set.remove(newString);
+                        }
+                    }
+                    currArray[j] = originalChar;
+                }
+            }
+            level++;
+        }
+        return 0;
+    }
+}
