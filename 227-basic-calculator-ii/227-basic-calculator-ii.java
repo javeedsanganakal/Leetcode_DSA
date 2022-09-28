290d3d1181e724adfb5d79dc0e4780d419066af9
Javeed Sanganakal <21953322+javeedsanganakal@users.noreply.github.com>
2022-09-28 15:01:47 -0700

    Time: 30 ms (39.95%), Space: 43.4 MB (84.39%) - LeetHub

--- 227-basic-calculator-ii/227-basic-calculator-ii.java
+++ 227-basic-calculator-ii/227-basic-calculator-ii.java
@@ -1,58 +1,51 @@
-//Approach - 1  : Stack
-//Time Complexity : O(n) length of the string
-//Space Complexity : O(n)
+//Approach - 1: Without Stack
+//Time Complexity : O(n)
+//Space Complexity : O(1)
+
 class Solution {
     public int calculate(String s) {
-
+        
         if(s == null || s.length()  == 0) return 0;
         
-        
-        
-        //Intailly LastSign and curr + and 0  asssume
         char lastSign = '+';
         int curr = 0;
         
-        Stack<Integer> st = new Stack<>();
-
-
+        int calc = 0;
+        int tail = 0;
+        
         for(int i=0; i< s.length(); i++){
             
             char c = s.charAt(i);
             
-            
-            //Case 1: Digit
-            //if its  digit assign to curr
+            //Case 1:
             if(Character.isDigit(c)){
                 //ASCII Value of 3 and 0 // 51 - 48
-                curr = curr*10 + c-'0'; // if s = "42"
+                curr = curr*10 + c-'0';
             }
             
-        
-            
-            //Case 2: Operator
-            if((!Character.isDigit(c) && c != ' ') || i == s.length()-1){
+            //Case 2:
+            if((!Character.isDigit(c) && c !=  ' ') || i == s.length()-1){
                 if(lastSign == '+'){
-                    st.push(curr);
+                    calc = calc + curr;
+                    tail = curr;
                 }
                 else if(lastSign == '-'){
-                    st.push(-curr);
+                    calc = calc-curr;
+                    tail = -curr;
                 }
                 else if(lastSign == '*'){
-                    st.push(st.pop()*curr);
+                    calc = calc - tail + (tail*curr);
+                    tail = tail*curr;
                 }
                 else {
-                    st.push(st.pop()/curr);
+                    calc = calc - tail + (tail/curr);
+                    tail = tail/curr;
                 }
                 curr = 0;
                 lastSign = c;
             }
             
         }
-        int result = 0; 
-
-        while(!st.empty()){
-            result += st.pop();
-        }
-        return result;
+        return calc;
     }
 }
