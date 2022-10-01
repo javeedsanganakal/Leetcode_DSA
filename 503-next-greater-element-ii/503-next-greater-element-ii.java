2b474e0bdb7b808f8298e23aaef32c1c0c5c1662
Javeed Sanganakal <21953322+javeedsanganakal@users.noreply.github.com>
2022-10-01 06:30:18 -0700

    Time: 24 ms (86.42%), Space: 42.8 MB (99.74%) - LeetHub

--- 503-next-greater-element-ii/503-next-greater-element-ii.java
+++ 503-next-greater-element-ii/503-next-greater-element-ii.java
@@ -1,23 +1,24 @@
-//Approach - 1: Brute Force, 2 Arrays
-//Time Complexity : O(n^2)
-//Space Complexity :  O(n); extra space for 2n array
+//Approach - 2: Monotonic Dec Stack
+//Time Complexity : O(n)
+//Space Complexity :  O(n)
 
 public class Solution {
     public int[] nextGreaterElements(int[] nums) {
-        int[] res = new int[nums.length];
-        int[] doublenums = new int[nums.length * 2];
+        int n = nums.length;
+        int[] res = new int[n];
+
+        Arrays.fill(res,-1);
         
-        System.arraycopy(nums, 0 , doublenums, 0, nums.length);
-        System.arraycopy(nums, 0 , doublenums, nums.length, nums.length);
+        Stack<Integer> st = new Stack<>();
         
-        for(int i=0; i<nums.length; i++){
-            res[i] = -1;
-            for(int j=i+1; j<doublenums.length; j++){
-                if(doublenums[j] > doublenums[i]){
-                    res[i] = doublenums[j];
-                    break;
-                }
+        
+        for(int i=0; i<2*n; i++){
+            while(!st.isEmpty() && nums[i%n] > nums[st.peek()]){
+                int popedIndex = st.pop();
+                res[popedIndex] = nums[i%n];
             }
+            if(i<n)
+             st.push(i);
         }
         return res;
     }
