73f38472b0a70f915af80ca9902da15d8535d4e2
Javeed Sanganakal <21953322+javeedsanganakal@users.noreply.github.com>
2022-10-01 05:30:12 -0700

    Time: 155 ms (59.54%), Space: 135.1 MB (5.60%) - LeetHub

--- 739-daily-temperatures/739-daily-temperatures.java
+++ 739-daily-temperatures/739-daily-temperatures.java
@@ -1,24 +1,23 @@
-//Approach - 1: Stack, push index and compare temp, pop() the peek, if curr day warmer than peek, and update the peek index
-//Time Complexity : O(n)
+//Approach - 2: Monotonic Dec Stack
+//Time Complexity : O(n) 
 //Space Complexity : O(n)
 
 class Solution {
     public int[] dailyTemperatures(int[] temperatures) {
         int n = temperatures.length;
-        int[] answer = new int[n];
-        Stack<Integer> stack = new Stack<>();
         
-        for (int currDay = 0; currDay < n; currDay++) {
-            int currentTemp = temperatures[currDay];
-            // Pop until the current day's temperature is not
-            // warmer than the temperature at the top of the stack
-            while (!stack.isEmpty() && temperatures[stack.peek()] < currentTemp) {
-                int prevDay = stack.pop();
-                answer[prevDay] = currDay - prevDay;
+        int[] result = new int[n];
+        
+        //Monotonic Decreasing Stack 4,3,2,1
+        Stack<Integer> st = new Stack<>();
+        
+        for(int i=0; i<n; i++){
+            while(!st.isEmpty() && temperatures[i] > temperatures[st.peek()]){
+                int poppedIndex = st.pop();
+                result[poppedIndex] = i - poppedIndex;
             }
-            stack.push(currDay);
+            st.push(i);
         }
-        
-        return answer;
+        return result;
     }
 }
