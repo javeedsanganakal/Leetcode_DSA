98ebc778614abc9c52a08ff06a43649e460d8d2c
Javeed Sanganakal <21953322+javeedsanganakal@users.noreply.github.com>
2022-10-12 09:58:24 -0700

    Time: 230 ms (32.64%), Space: 97.3 MB (15.59%) - LeetHub

--- 0301-remove-invalid-parentheses/0301-remove-invalid-parentheses.java
+++ 0301-remove-invalid-parentheses/0301-remove-invalid-parentheses.java
@@ -1,51 +1,50 @@
-//Approach - 1: BSF, Set
+//Approach - 1: DFS, Set, max
 //Time Complexity : 
 //Space Complexity : 
 
 //Notes
 class Solution {
+    List<String> result;
+    Set<String> set;
+    int max;
     public List<String> removeInvalidParentheses(String s) {
-        List<String> result = new ArrayList<>();
+        result = new ArrayList<>();
+        set = new HashSet<>();
         if(s == null || s.length()  == 0) return result;
+        max = 0;
         
-        boolean flag = false;
-        
-        Queue<String> q = new LinkedList<>();
-        q.add(s);
-        
-        HashSet<String>  set = new HashSet<>();
-        set.add(s);
-        
-        while(!q.isEmpty()){
-            
-            //g=Going through all strings in that level 
-            int size = q.size();
-            
-            for(int i=0; i<size; i++){
-                String curr = q.poll();
-                
-                if(isValid(curr)){
-                    flag = true;
-                    result.add(curr);
-                }
-                else{
-                    if(!flag){
-                        for(int j=0; j<curr.length(); j++){
-                            //remove one brace and add it to set
-                            if(Character.isLetter(curr.charAt(j))) continue;
-                            String child = curr.substring(0,j) + curr.substring(j+1);
-                            if(!set.contains(child)){
-                                q.add(child);
-                                set.add(child);
-                            }
-                        }
-                    }
-                }
-                
-            }
-            
-        }
+        dfs(s);
         return result;
+    }
+    
+    private void dfs(String str){
+        //base 
+        if(str.length() < max) return;
+        if(isValid(str)){
+            if(str.length() > max){
+                result = new ArrayList<>();
+                max = str.length();
+            }
+            result.add(str);
+            return;
+        }
+        //logic
+        
+        //add
+        if(isValid(str)){
+            result.add(str);
+        }
+        set.add(str);
+
+        for(int i=0; i<str.length(); i++){
+            char c = str.charAt(i);
+            if(Character.isLetter(c)) continue;
+            String child = str.substring(0,i) + str.substring(i+1);
+            if(!set.contains(child)){
+                set.add(child);
+                dfs(child);
+            }
+        }
     }
     
     private boolean isValid(String str){
