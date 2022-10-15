62349766945e79dc13ff436da8db9082b06c60fb
Javeed Sanganakal <21953322+javeedsanganakal@users.noreply.github.com>
2022-10-14 18:54:17 -0700

    Time: 13 ms (36.67%), Space: 88.1 MB (80.26%) - LeetHub

--- 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.java
+++ 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.java
@@ -4,42 +4,21 @@
  
 class Solution {
     public int minDominoRotations(int[] tops, int[] bottoms) {
-        HashMap<Integer, Integer> map = new HashMap<>();
         
         int n = tops.length;
         //Element which you have to replcae
         int candidate = -1;
+        int result = check(tops, bottoms, tops[0]);
+        if(result != -1) return result;
+        return check(tops, bottoms, bottoms[0]);
+    
         
-        //1st Pass
-        for(int i=0; i<n; i++){
-            
-            int top = tops[i];
-            map.put(top, map.getOrDefault(top,0) + 1);
-            int countTop = map.get(top);
-        
-            if(countTop >= n){
-                //We found candidate element for replacing
-                candidate = top;
-                break;
-            }
-            
-            int bottom = bottoms[i];
-            map.put(bottom, map.getOrDefault(bottom,0) + 1);
-            int countBottom = map.get(bottom);
-        
-            if(countBottom >= n){
-                //We found candidate element for replacing
-                candidate = bottom;
-                break;
-            }
-        }
-        if(candidate == -1) return -1;
-        
-        
-        //2nd Pass
+    }
+    
+    private int check(int[] tops, int[] bottoms, int candidate){
         int topRotation = 0;
         int bottomRotation = 0;
-        for(int i=0; i<n; i++){
+        for(int i=0; i<tops.length; i++){
             if(candidate != tops[i] && candidate != bottoms[i]) return -1;
             if(tops[i] != candidate) topRotation ++;
             if(bottoms[i] != candidate) bottomRotation ++;
