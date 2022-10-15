2d3ef6c16f95a6ae47b929997317101054dbd53a
Javeed Sanganakal <21953322+javeedsanganakal@users.noreply.github.com>
2022-10-15 12:43:31 -0700

    Time: 6 ms (68.49%), Space: 40.3 MB (91.65%) - LeetHub

--- 1055-shortest-way-to-form-string/1055-shortest-way-to-form-string.java
+++ 1055-shortest-way-to-form-string/1055-shortest-way-to-form-string.java
@@ -1,5 +1,5 @@
-//Approach - 2: 2 Pointers, HahsMap, BinarySearch
-//Time Complexity : O(nlog(k))
+//Approach - 1: 2. Pointers, HashSet
+//Time Complexity : O(m*n)
 //Space Complexity : O(1)
 
 class Solution {
@@ -8,52 +8,34 @@ class Solution {
         int sourceLen = source.length();
         int targetLen = target.length();
         
-        HashMap<Character, List<Integer>> map = new HashMap<>();
-        //Add all the chars in Source to HashMap
+        HashSet<Character> set = new HashSet<>();
         for(int i=0; i<sourceLen; i++){
-            char c = source.charAt(i);
-            if(!map.containsKey(c)){
-                map.put(c, new ArrayList<>());
-            }
-            map.get(c).add(i);
+            set.add(source.charAt(i));
         }
         
         int targetPointer = 0;
         int sourcePointer = 0;
         int count = 1;
-        
         while(targetPointer < targetLen){
+            char sChar = source.charAt(sourcePointer);
             char tChar = target.charAt(targetPointer);
-            if(!map.containsKey(tChar)) return -1;
             
-            List<Integer> li = map.get(tChar); 
+            if(!set.contains(tChar)) return -1;
             
-            int k = Collections.binarySearch(li, sourcePointer);
-            
-            if(k<0){
-                //if the element is not in the list
-                //sourcePointer = 13
-                //[0,1,3,4,17,19] => Collection gives k = -5 ==> i.e, -4-1
-                //k = -k-1 ; -(-5)-1 = 4
-                // At 4th index 13 can be inserted
-                
-                //sourcePointer = 19
-                //[0,1,3,4,17,19] => k = 5
-                //Gives exact postive index of it. 
-                
-                k = -k-1;
-                
-            }
-            if(k == li.size()){
-                count++;
-                sourcePointer = li.get(0);    
+            if(sChar == tChar){
+                targetPointer++;
+                sourcePointer++;
+                if(targetPointer == targetLen) return count;
             }
             else{
-                //if the element is in the list
-                sourcePointer = li.get(k);
-            } 
-            sourcePointer++;
-            targetPointer++;
+                sourcePointer++;
+            }
+            
+            if(sourcePointer == sourceLen){
+                //reset the count, after froming a subsequence
+                sourcePointer = 0;
+                count++;
+            }
         }
         return count;
     }
